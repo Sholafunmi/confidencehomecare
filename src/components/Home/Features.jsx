7e5f3997e7eee4d36b62db9bed0ac325cb9f7fbd
Sholafunmi <sholafunmiadewara@gmail.com>
2024-02-06 07:18:53 +0100

    links

--- src/components/Home/Features.jsx
+++ src/components/Home/Features.jsx
@@ -24,26 +24,25 @@ const Features = () => {
                             </div>
                         </Link> */}
 
-                        {/* <Link to='/services' onClick={scrollToTop} className='md:w-[28%]'> */}
+                        <Link to='/services'>
                           <div name='servicebox' className="aboutbox flex-col w-[95%] md:w-[28%] h-[350px] mb-5 md:mb-0 ml-[2.5%] md:ml-0 border-8 border-[#b69d11]  ">
                             <div className="servicesTop h-[80%] w-full flex">
                                 
                             </div>
 
                             <div className='servicesBottom  h-[20%] w-full flex justify-center text-[#442572] bg-[#fff] '>
-                              <a href="/services"  onClick={scrollToTop} className='flex items-center font-bold text-[30px]' >
+                              <p className='flex items-center font-bold text-[30px]' >
                                 Services
-                               
-                              </a>
+                               </p>
                                 
                             </div>
 
                           </div>
 
-                        {/* </Link> */}
+                        </Link>
                        
-                        <Link to='/about' onClick={scrollToTop} className='md:w-[28%]'>
-                          <div name='aboutbox' className="aboutbox flex-col w-[95%] h-[350px] mb-5 md:mb-0 ml-[2.5%] md:ml-0 border-8 border-[#b69d11]  ">
+                        <Link to='/about' >
+                          <div name='aboutbox' className="aboutbox flex-col w-[95%] h-[350px] md:w-[28%] mb-5 md:mb-0 ml-[2.5%] md:ml-0 border-8 border-[#b69d11]  ">
                                 <div className="aboutTop h-[80%] w-full flex">
                                     
                                 </div>
@@ -57,27 +56,25 @@ const Features = () => {
 
 
 
-                        <Link to='/contact' onClick={scrollToTop} className='md:w-[28%]'>
-                          <div name='contactbox' className="contactbox flex-col w-[95%] h-[350px] md:w-[] mb-5 md:mb-0 ml-[2.5%] md:ml-0 border-8 border-[#b69d11] ">
+                        <Link to='/contact'>
+                          <div name='contactbox' className="contactbox flex-col w-[95%] h-[350px] md:w-[28%] mb-5 md:mb-0 ml-[2.5%] md:ml-0 border-8 border-[#b69d11] ">
                                 <div className="contactTop h-[80%] w-full flex "href="/contact">
                                 
                                 </div>
 
                                 <div className='contactBottom  h-[20%] w-full flex justify-center text-[#442572] bg-[#fff] '>
-                                    <p className='flex items-center font-bold text-[30px] ' href="/contact">Contact Us</p>
+                                    <p className='flex items-center font-bold text-[30px] '>Contact Us</p>
                                 </div>
 
                           </div>
                         </Link> 
 
                           {/* this works  */}
-                        <Link to='/services'>
+                        {/* <Link to='/services'>
                           Serv
-                        </Link>
-
-                        <a href="/services">
-                          servee
-                         </a>
+                        </Link> */}
+                        
+                       
 
               
                    
