eb25d23a8889b61f03f11341675cead22c79826a
Sholafunmi <sholafunmiadewara@gmail.com>
2024-02-06 11:05:00 +0100

    features link

--- src/components/Home/Features.jsx
+++ src/components/Home/Features.jsx
@@ -9,40 +9,29 @@ const Features = () => {
   };
 
   return (
-    <div className='features w-full h-fit md:flex justify-center items-center mt-14 py-6 md:py-20 md:gap-10 ' >
-         
-                        {/* <Link className=' w-[95%]  md:w-[28%] h-[350px]  '   to="/services"  smooth={true} offset={50} duration={500} >
-                          <div name='servicebox' className="servicebox flex-col h-full  w-full mb-5 md:mb-0 ml-[2.5%] md:ml-0 border-8 border-[#d5796d] ">
-                              <div className="servicesTop h-[80%] w-full flex"></div>
+    <div className='features w-full h-fit flex flex-col md:flex-row justify-center items-center mt-14 py-6 md:py-20 md:gap-10 ' >
+                        <div className=' w-[95%] md:w-[28%] '>
+                            <Link to='/services'>
+                            <div name='servicebox' className="aboutbox flex flex-col w-[95%] md:w-full  h-[350px] mb-5 md:mb-0 ml-[2.5%] md:ml-0 border-8 border-[#b69d11]  ">
+                              <div className="servicesTop h-[80%] w-full flex">
+                                  
+                              </div>
 
-                              <div className='servicesBottom h-[20%] w-full flex justify-center text-[#442572] bg-[#fff] '>
-                              
-                                  <p className='flex items-center font-bold text-[20px] cursor-pointer '   to="vision"  smooth={true} offset={50} duration={500} >
-                                  Our Services </p>
+                              <div className='servicesBottom  h-[20%] w-full flex justify-center text-[#442572] bg-[#fff] '>
+                                <p className='flex items-center font-bold text-[30px]' >
+                                  Services
+                                </p>
+                                  
                               </div>
 
                             </div>
-                        </Link> */}
 
-                        <Link to='/services'>
-                          <div name='servicebox' className="aboutbox flex-col w-[95%] md:w-[28%] h-[350px] mb-5 md:mb-0 ml-[2.5%] md:ml-0 border-8 border-[#b69d11]  ">
-                            <div className="servicesTop h-[80%] w-full flex">
-                                
-                            </div>
-
-                            <div className='servicesBottom  h-[20%] w-full flex justify-center text-[#442572] bg-[#fff] '>
-                              <p className='flex items-center font-bold text-[30px]' >
-                                Services
-                               </p>
-                                
-                            </div>
-
-                          </div>
-
-                        </Link>
-                       
+                          </Link>
+                        </div>
+                        
+                        <div className=' w-[95%] md:w-[28%] '>
                         <Link to='/about' >
-                          <div name='aboutbox' className="aboutbox flex-col w-[95%] h-[350px] md:w-[28%] mb-5 md:mb-0 ml-[2.5%] md:ml-0 border-8 border-[#b69d11]  ">
+                          <div name='aboutbox' className="aboutbox flex flex-col w-[95%] md:w-full h-[350px] mb-5 md:mb-0 ml-[2.5%] md:ml-0 border-8 border-[#b69d11]  ">
                                 <div className="aboutTop h-[80%] w-full flex">
                                     
                                 </div>
@@ -53,11 +42,12 @@ const Features = () => {
 
                           </div>
                         </Link>
+                      </div>
 
 
-
+                      <div className=' w-[95%] md:w-[28%] '>
                         <Link to='/contact'>
-                          <div name='contactbox' className="contactbox flex-col w-[95%] h-[350px] md:w-[28%] mb-5 md:mb-0 ml-[2.5%] md:ml-0 border-8 border-[#b69d11] ">
+                          <div name='contactbox' className="contactbox flex flex-col w-[95%] h-[350px] md:w-full mb-5 md:mb-0 ml-[2.5%] md:ml-0 border-8 border-[#b69d11] ">
                                 <div className="contactTop h-[80%] w-full flex "href="/contact">
                                 
                                 </div>
@@ -67,7 +57,8 @@ const Features = () => {
                                 </div>
 
                           </div>
-                        </Link> 
+                        </Link>
+                        </div>
 
                           {/* this works  */}
                         {/* <Link to='/services'>
